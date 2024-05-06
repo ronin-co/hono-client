cd373ce308cd240bd2cacc8899fe69aa2580a1f7
Leo Lamprecht <leo.lamprecht@icloud.com>
2024-05-06 12:33:09 +0200

    Added Hono client (#1)
    * Added Hono client
    
    * Bumped version

---
+++ tests/integration/index.test.ts
@@ -0,0 +1,101 @@
+import { describe, expect, it } from "bun:test";
+import { createFactory } from "hono/factory";
+import { testClient } from "hono/testing";
+
+import { ronin } from "@/index";
+
+import type { Bindings, Variables } from "@/index";
+
+describe("use `ronin` middleware", () => {
+  const factory = createFactory<{ Bindings: Bindings; Variables: Variables }>();
+
+  it("with a missing `RONIN_TOKEN`", async () => {
+    const app = factory
+      .createApp()
+      .use("*", ronin())
+      .get("/", async (c) => {
+        expect(c.var.ronin).toBeDefined();
+
+        const posts = (await c.var.ronin.get.blogPosts()) as Array<unknown>;
+
+        return c.json(posts);
+      });
+
+    const response = await testClient(app, {
+      RONIN_TOKEN: null,
+    }).index.$get();
+
+    expect(response.ok).toBe(false);
+    expect(response.status).toBe(500);
+  });
+
+  it("with default options", async () => {
+    const app = factory
+      .createApp()
+      .use("*", ronin())
+      .get("/", async (c) => {
+        expect(c.var.ronin).toBeDefined();
+
+        const users = await c.var.ronin.get.users();
+
+        expect(users).toBeDefined();
+        expect(users).toBeInstanceOf(Array);
+
+        return c.json(users);
+      });
+
+    const response = await testClient(app, {
+      RONIN_TOKEN: import.meta.env.RONIN_TOKEN,
+    }).index.$get();
+
+    expect(response.ok).toBe(true);
+    expect(response.status).toBe(200);
+
+    const json = await response.json();
+
+    expect(json).toBeDefined();
+    expect(json).toBeInstanceOf(Array);
+    expect(json).toHaveLength(0);
+  });
+
+  it("with a custom options", async () => {
+    const app = factory
+      .createApp()
+      .use(
+        "*",
+        ronin({
+          hooks: {
+            user: {
+              beforeGet: (query) => {
+                query.limitedTo = 1;
+                return query;
+              },
+            },
+          },
+        }),
+      )
+      .get("/", async (c) => {
+        expect(c.var.ronin).toBeDefined();
+
+        const users = await c.var.ronin.get.users();
+
+        expect(users).toBeDefined();
+        expect(users).toBeInstanceOf(Array);
+
+        return c.json(users);
+      });
+
+    const response = await testClient(app, {
+      RONIN_TOKEN: import.meta.env.RONIN_TOKEN,
+    }).index.$get();
+
+    expect(response.ok).toBe(true);
+    expect(response.status).toBe(200);
+
+    const json = await response.json();
+
+    expect(json).toBeDefined();
+    expect(json).toBeInstanceOf(Array);
+    expect(json).toHaveLength(1);
+  });
+});
---
+++ tests/preload.ts
@@ -0,0 +1,30 @@
+import { afterAll, afterEach, beforeAll } from "bun:test";
+import { setupServer } from "msw/node";
+import { http, HttpResponse } from "msw";
+
+import.meta.env.RONIN_TOKEN = "secret-token";
+
+const mockHttpServer = setupServer(
+  http.post("https://data.ronin.co/", async ({ request }) => {
+    const queries = (await request.json()) as {
+      queries: Array<{
+        get: {
+          users: {
+            limitedTo?: number;
+          };
+        };
+      }>;
+    };
+
+    const numRecords = queries?.queries?.[0].get?.users?.limitedTo || 0;
+    const records = Array.from({ length: numRecords }).fill(null);
+
+    return HttpResponse.json({
+      results: [{ records }],
+    });
+  }),
+);
+
+beforeAll(() => mockHttpServer.listen());
+afterEach(() => mockHttpServer.resetHandlers());
+afterAll(() => mockHttpServer.close());
